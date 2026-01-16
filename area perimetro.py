2f525a52eeb7d532330dd6464f63525a7378d0ff
flavex-creator <flaviodanielferreira0@gmail.com>
2026-01-16 15:18:02 +0000

    Add files via upload

---
+++ area perimetro.py
@@ -0,0 +1,13 @@
+
+from math import pi
+
+raio = int(input("Insira o valor do raio: "))
+
+
+perimetro = 2 * pi * raio
+
+
+area = pi * raio ** 2
+
+print("O perímetro do círculo é:", round(perimetro, 2))
+print("A área do círculo é:", round(area, 2))
