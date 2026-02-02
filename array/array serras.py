9b45af0b2aa51f7ae916f89794b75fab9d259ea2
flavex-creator <flaviodanielferreira0@gmail.com>
2026-02-02 09:26:05 +0000

    Add files via upload

---
+++ array/array serras.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Array criado a partir de uma list
+print('Serras de Portugal')
+serras = np.array(["Estrela", "Monchique"])
+print(serras)
+
+"""Array criado a partir de uma tuple
+e tipo de dados float"""
+print('Portugal em km')
+portugal_km = np.array((561, 218, 832, 1215), dtype=float)
+print(portugal_km)
